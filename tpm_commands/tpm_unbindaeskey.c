f0498f2b37e7f6a70eeefb1fc5c3c860d5d321af
Animesh Patel <animeshx.patel@intel.com>
2017-01-13 11:54:45 +0530

    dev-cit-2.x-tpm2-windows-vm-attestation-support: Change log: Sign data and unbind aes key modified to first import the key and then perform the operations

--- tpm_commands/tpm_unbindaeskey.c
+++ tpm_commands/tpm_unbindaeskey.c
@@ -1,42 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define tpmtools_exe "\"C:\\Program Files (x86)\\Intel\\TrustAgent\\bin\\TPMTool.exe\""
+
 int main(int argc, char** argv) {
     int     i, exitCode = -1;
-    char    *input_file = NULL, *key_name = NULL, *output_file = NULL, *key_auth;
+    char    *input_file = NULL, *key_name = NULL, *output_file = NULL, *key_auth = NULL, *blob_file = NULL;
     char	command0[2304]={0};
 
-    printf("Parsing arguments to identify tpm command and required parameters");
     for (i = 0; i < argc; i++) {
         if ( argv[i] && strcmp(argv[i], "-i") == 0 ) {
             input_file = argv[i+1];
-            printf("input_file : %s\n", input_file);
         }
         if ( argv[i] && strcmp(argv[i], "-k") == 0 ) {
             key_name = argv[i+1];
-            printf("key_name : %s\n", key_name);
         }
         if ( argv[i] && strcmp(argv[i], "-o") == 0 ) {
             output_file = argv[i+1];
-            printf("output_file : %s\n", output_file);
         }
 		if ( argv[i] && strcmp(argv[i], "-q") == 0 ) {
             key_auth = argv[i+1];
-            printf("key_auth : %s\n", key_auth);
+        }
+		if ( argv[i] && strcmp(argv[i], "-b") == 0 ) {
+            blob_file = argv[i+1];
         }
     }
 
-	if(output_file != NULL)
-		sprintf_s(command0,sizeof(command0),"TPMTool.exe decrypt %s %s %s > %s", key_name, input_file, key_auth, output_file);
-	else
-		sprintf_s(command0,sizeof(command0),"TPMTool.exe decrypt %s %s %s", key_name, input_file, key_auth);
-	printf("Unbinding Command : %s\n", command0);
-
+	output_file = (output_file == NULL) ? "" : output_file;
+	sprintf_s(command0,sizeof(command0),tpmtools_exe" importaik %s %s", blob_file, key_name);
 	i = system(command0);
-	printf("system call to tpm_unbindaeskey command exit status : %d\n", i);
-    if(i != 0)
-		exitCode = -1;
-    exitCode = 0;
+	if ( i == 0 ) {
+		sprintf_s(command0,sizeof(command0),tpmtools_exe" unbind %s %s %s %s", key_name, input_file, key_auth, output_file);
+		i = system(command0);
+		if ( i == 0 )
+			exitCode = 0;
+	}
 
     return exitCode;
 }
